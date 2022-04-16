422c2140d0d32f850acfcd381ad8ea73727c6cd7
Anuka <arrinsamurai7@gmail.com>
2022-04-16 18:06:18 +0530

    Header creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-
+import React from "react";
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1>Welcome To DinoTech🦖</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To DinoTech🦖</h1>
+        </Container>
+
+      </main>
+      <Footer />
     </>
   );
 }
