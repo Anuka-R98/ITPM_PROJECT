ae9c34b3dbc18ba2af29a62c578eb2b3a4e18505
NDilsh <nadun31dilshan@gmail.com>
2022-04-21 13:20:11 +0530

    IT20191788-Add App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,31 @@ import React from "react";
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from './screens/HomeScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
+import OrderListScreen from './screens/OrderListScreen'
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome To Dino-Tech</h1>
+        <Route path='/order/:id' component={OrderScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+
+          <Route path='/admin/orderlist' component={OrderListScreen} />
+
         </Container>
 
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
