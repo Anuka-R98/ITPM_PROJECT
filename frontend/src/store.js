911c2721152e6f359e025f244ac01aefce7aac64
shehaniperer <shehaniaperera.96@gmail.com>
2022-05-06 01:03:00 +0530

    update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-// import {
-//   productListReducer,
-//   productDetailsReducer,
-//   productDeleteReducer,
-//   productCreateReducer,
-//   productUpdateReducer,
-//   productReviewCreateReducer,
-//   productTopRatedReducer,
-// } from './reducers/productReducers'
+import {
+  productListReducer,
+  productDetailsReducer,
+  productDeleteReducer,
+  productCreateReducer,
+  productUpdateReducer,
+  productReviewCreateReducer,
+  productTopRatedReducer,
+} from './reducers/productReducers'
 // import { cartReducer } from './reducers/cartReducers'
 import {
   userLoginReducer,
@@ -30,13 +30,13 @@ import {
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
-  // productList: productListReducer,
-  // productDetails: productDetailsReducer,
-  // productDelete: productDeleteReducer,
-  // productCreate: productCreateReducer,
-  // productUpdate: productUpdateReducer,
-  // productReviewCreate: productReviewCreateReducer,
-  // productTopRated: productTopRatedReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
+  productTopRated: productTopRatedReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
