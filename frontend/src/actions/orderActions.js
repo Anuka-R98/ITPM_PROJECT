67c009da7a2377e4147c1739c9d050cf79599886
NDilsh <nadun31dilshan@gmail.com>
2022-05-06 01:32:08 +0530

    delete function

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -19,6 +19,10 @@ import {
   ORDER_DELIVER_FAIL,
   ORDER_DELIVER_SUCCESS,
   ORDER_DELIVER_REQUEST,
+
+  ORDER_DELETE_SUCCESS,
+  ORDER_DELETE_REQUEST,
+  ORDER_DELETE_FAIL,
 } from '../constants/orderConstants'
 import { logout } from './userActions'
 
@@ -257,6 +261,42 @@ export const listOrders = () => async (dispatch, getState) => {
     }
     dispatch({
       type: ORDER_LIST_FAIL,
+      payload: message,
+    })
+  }
+}
+
+export const deleteOrder = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DELETE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    await axios.delete(`/api/orders/${id}`, config)
+
+    dispatch({
+      type: ORDER_DELETE_SUCCESS,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: ORDER_DELETE_FAIL,
       payload: message,
     })
   }
