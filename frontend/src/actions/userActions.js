8b3382030e502b066843b81e76085d4ac0dbb6ec
Anuka <arrinsamurai7@gmail.com>
2022-05-08 19:08:20 +0530

    Updates to user action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -194,7 +194,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
   }
 }
 
-export const listUsers = () => async (dispatch, getState) => {
+export const listUsers = (keyword = '') => async (dispatch, getState) => {
   try {
     dispatch({
       type: USER_LIST_REQUEST,
@@ -210,7 +210,7 @@ export const listUsers = () => async (dispatch, getState) => {
       },
     }
 
-    const { data } = await axios.get(`/api/users`, config)
+    const { data } = await axios.get(`/api/users?keyword=${keyword}`, config)
 
     dispatch({
       type: USER_LIST_SUCCESS,
